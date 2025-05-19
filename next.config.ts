d5c638d2c41d611a98e551fde872c80b1d870830
Marion Manalo <marionmanalo682@gmail.com>
2025-05-19 00:43:45 -0400

    Restored next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,6 @@ const nextConfig: NextConfig = {
 };
 
 module.exports = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
