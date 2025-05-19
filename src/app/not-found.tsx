7485f98f76824b5b6d7b48907e7edc45b96f809c
Marion Manalo <marionmanalo682@gmail.com>
2025-05-19 00:48:52 -0400

    Minor changes added to not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
         <div className="flex flex-col items-center justify-center min-h-screen bg-white px-4 text-center">
             <h1 className="text-5xl font-bold text-gray-800 mb-4">404</h1>
             <p className="text-lg text-gray-600 mb-6">
-                Sorry, the page you're looking for doesn't exist.
+                Sorry, the page you&apos;re looking for doesn&apos;t exist.
             </p>
             <Link href="/">
                 <div className="inline-block px-6 py-3 bg-blue-600 text-white font-medium rounded-md hover:bg-blue-700 transition">
